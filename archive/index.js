8d859690e3eaf279f68198ed24176ea8d004ae51
github.user-at-576386355408 <brandoncookedev@gmail.com>
2018-04-11 13:42:00 -0400

    Created npm project and published

--- archive/index.js
+++
@@ -1,6 +0,0 @@
-exports.Set         = require('./lib/Set');
-exports.Event       = require('./lib/Event');
-exports.Phase       = require('./lib/Phase');
-exports.Player      = require('./lib/Player');
-exports.Tournament  = require('./lib/Tournament');
-exports.PhaseGroup  = require('./lib/PhaseGroup');
